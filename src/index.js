0d32ba7a28b78e4cc33fa88b5cf6fcaa7a1349a0
DenisaNicoletaH <denisanhategan@gmail.com>
2023-04-17 14:13:54 -0400

    Added title
    small useless change

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+export default function MyApp() {
+  <h1>FrontEnd Title</h1>
+
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
