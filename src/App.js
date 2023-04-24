a834bfd1986d0233efcdfe157994ffeb9af381c9
DenisaNicoletaH <denisanhategan@gmail.com>
2023-04-24 15:47:17 -0400

    Buttomn,form and input done---> removed ScrollView

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import ChatRoom from './ChatRoom';
 
 function App() {
   return (
     <div>
-    
+    <ChatRoom></ChatRoom>
     </div>
   );
 }
--- src/ChatRoom.js
+++ src/ChatRoom.js
@@ -1,24 +1,53 @@
 import React, {useState, useEffect } from 'react';
+import axios from 'axios';
+import { FlatList, View, Text,ScrollView } from 'react';
+
 
 function ChatRoom() {
-   const sendMessage = (message) => {
-    axios.post('/api/messages', message)
+  const [messages, setMessages] = useState([]);
+  const [newMessages, setNewMessages] = useState('');
+
+  
+  const sendMessage = (event) => {
+    event.preventDefault();
+    axios.post('/api/messages',{ message : newMessages})
       .then(response => setMessages([...messages, response.data]))
       .catch(error => console.error(error));
+      setMessages('');
   };
   
   function sendMessages(){
     alert("You have sent a message")
   } 
-  <button onClick={sendMessages}>Send Message</button>
- 
-  return (
-    <div>
 
-     
-      {/* Render chat messages here /}
-      {/ Render input field and send button here */}
-    </div>
+  return (
+    
+    <div>
+      
+      
+        <form onSubmit={sendMessage}>
+        <input
+        type="text"
+        value={newMessages}
+        onChange={(event) => setNewMessages(event.target.value)}/>
+
+      
+      </form>
+      <button onClick={sendMessages}>Send Message</button>
+
+      {/* Render chat messages here */}
+      {/* Render input field and send button here */}
+      <ul>
+        {messages.map((message, index) => (
+          <li key={index}>{message}</li>
+        ))}
+      </ul>
+      
+ 
+
+    
+     </div>
   );
-  }
+}
+
 export default ChatRoom;
