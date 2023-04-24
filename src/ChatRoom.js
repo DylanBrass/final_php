49c0c73dc4c631626c6e0163db97f05f55b80c11
Mila5847 <46633364+Mila5847@users.noreply.github.com>
2023-04-24 14:57:18 -0400

    initial push

---
+++ src/ChatRoom.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect } from 'react';
+
+function ChatRoom() {
+    const [messages, setMessages] = useState([]);
+
+    return (
+      <div>
+        {/* Render chat messages here */}
+        {/* Render input field and send button here */}
+      </div>
+    );
+}
+
+export default ChatRoom;
