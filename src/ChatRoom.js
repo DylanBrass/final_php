c02bbfbecf5eb98bf0d5504d3713fae7149feee8
DenisaNicoletaH <denisanhategan@gmail.com>
2023-04-24 15:09:37 -0400

    random Button

--- src/ChatRoom.js
+++ src/ChatRoom.js
@@ -1,14 +1,24 @@
 import React, {useState, useEffect } from 'react';
 
 function ChatRoom() {
-    const [messages, setMessages] = useState([]);
+   const sendMessage = (message) => {
+    axios.post('/api/messages', message)
+      .then(response => setMessages([...messages, response.data]))
+      .catch(error => console.error(error));
+  };
+  
+  function sendMessages(){
+    alert("You have sent a message")
+  } 
+  <button onClick={sendMessages}>Send Message</button>
+ 
+  return (
+    <div>
 
-    return (
-      <div>
-        {/* Render chat messages here */}
-        {/* Render input field and send button here */}
-      </div>
-    );
-}
-
+     
+      {/* Render chat messages here /}
+      {/ Render input field and send button here */}
+    </div>
+  );
+  }
 export default ChatRoom;
