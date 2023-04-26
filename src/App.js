55427a6d5fd2011b6722978e40cfa5e8d7731107
Mila5847 <46633364+Mila5847@users.noreply.github.com>
2023-04-26 00:28:42 -0400

    log in page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 import ChatRoom from './Components/ChatRoom';
 import ChatDisplay from './Components/ChatDisplay';
+import Login from './Components/Login';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLoginSuccess = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
     <div>
-      <ChatDisplay />
-      <ChatRoom />
+      {!isLoggedIn && <Login onLoginSuccess={handleLoginSuccess} />}
+      {isLoggedIn && (
+        <>
+          <ChatDisplay />
+          <ChatRoom />
+        </>
+      )}
     </div>
   );
 }
-
-
-
 
 export default App;
---
+++ src/Components/Login.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Login({ onLoginSuccess }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // TODO: Implement API call to authenticate user
+    axios
+      .post('http://127.0.0.1:8000/api/users/login', { username, password })
+      .then((response) => {
+        if (response.status === 200) {
+          console.log(response.data);
+          onLoginSuccess(); // Call onLoginSuccess when login is successful
+        }
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(event) => setUsername(event.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default Login;
